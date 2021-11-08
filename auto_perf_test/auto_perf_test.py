4f8790724e8385563bb030058ec9714117f8d5fc
Wenqi Jiang <wejiang@alveo0.ethz.ch>
2021-11-08 10:22:38 +0100

    more projects

--- auto_perf_test/auto_perf_test.py
+++ auto_perf_test/auto_perf_test.py
@@ -3,6 +3,7 @@
 # user can either specify a single recall goal, or evaluate all the recall goal stored in the dictionary
 # The output performance dict format:
 #   p[dbname][index_key][topK][recall_goal] = QPS
+# Note! The bitstream folder should contain xrt.ini that enables profiling storing profile_summary.csv
 # Copy this script to the FPGA bitstream folder to use it
 # Usage 1, specify recall:
 #    python auto_perf_test.py --dbname SIFT100M --topK 10 --use_recall_goal 1 --recall_goal 0.8 --nlist_min 1024 --nlist_max 16384 --FPGA_num 1 --bank_num 16 --recall_dict_dir './cpu_recall_index_nprobe_pairs_SIFT100M.pkl' --FPGA_perf_dict_dir './FPGA_perf_dict_SIFT100M.pkl' --overwrite 0
@@ -18,9 +19,14 @@
 #    |_ bitstream_K_10
 #    |_ bitstream_K_100
 #  In each bitstream folder, e.g., bitstream_K_1, run the evaluate all command, e.g.,
-#    python auto_perf_test.py --dbname SIFT100M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 1 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT100M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT100M.pkl' --overwrite 0
-#    python auto_perf_test.py --dbname SIFT500M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 4 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT500M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT500M.pkl' --overwrite 0
-#    python auto_perf_test.py --dbname SIFT1000M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 8 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT1000M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT1000M.pkl' --overwrite 0
+#    python ../auto_perf_test.py --dbname SIFT100M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 1 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT100M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT100M.pkl' --overwrite 1 > log_SIFT100M
+#    python ../auto_perf_test.py --dbname SIFT500M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 4 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT500M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT500M.pkl' --overwrite 1 > log_SIFT500M
+#    python ../auto_perf_test.py --dbname SIFT1000M --topK 1 --use_recall_goal 0 --nlist_min 1024 --nlist_max 65536 --FPGA_num 8 --bank_num 12 --recall_dict_dir '../cpu_recall_index_nprobe_pairs_SIFT1000M.pkl' --FPGA_perf_dict_dir '../FPGA_perf_dict_SIFT1000M.pkl' --overwrite 1 > log_SIFT1000M
+
+# Note! The recalls of the SITF500M & SIFT1000M are wrong, because
+#   1. the host verify the ground truth with the SIFT100M dataset (I hardcoded it)
+#   2. the results returned by this script only uses the data partition of 1 single FPGA, 
+#         we need to gather the partition of multiple FPGAs to verify the correctness
 
 import csv
 import os
@@ -135,15 +141,15 @@ for index_key in d_recall[dbname]:
         if s[:3]  == "IVF":
             nlist = int(s[3:])
         else:
-            raise ValueError
+            continue
     elif len(index_array) == 3: # "OPQ16,IVF4096,PQ16" or "OPQ16,IMI2x14,PQ16" 
         s = index_array[1]
         if s[:3]  == "IVF":
             nlist = int(s[3:])
         else:
-            raise ValueError
+            continue
     else:
-        raise ValueError
+        continue
     if nlist < nlist_min or nlist > nlist_max:
         continue
 
@@ -154,7 +160,7 @@ for index_key in d_recall[dbname]:
                 data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{bank_num}_banks".format(
                     dbname=dbname, index_key=index_key, bank_num=bank_num)
             else:
-                data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{FPGA_num}_{bank_num}_banks/FPGA0".format(
+                data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{FPGA_num}_FPGA_{bank_num}_banks/FPGA_0".format(
                     dbname=dbname, index_key=index_key, FPGA_num=FPGA_num, bank_num=bank_num)
             cmd = "./host vadd.xclbin {nlist} {nprobe} {OPQ_enable} {data_dir} /mnt/scratch/wenqi/saved_npy_data/gnd".format(
                 nlist=nlist, nprobe=nprobe, OPQ_enable=OPQ_enable, data_dir=data_dir)
@@ -171,7 +177,7 @@ for index_key in d_recall[dbname]:
                     data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{bank_num}_banks".format(
                         dbname=dbname, index_key=index_key, bank_num=bank_num)
                 else:
-                    data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{FPGA_num}_{bank_num}_banks/FPGA0".format(
+                    data_dir = "/mnt/scratch/wenqi/saved_npy_data/FPGA_data_{dbname}_{index_key}_{FPGA_num}_FPGA_{bank_num}_banks/FPGA_0".format(
                         dbname=dbname, index_key=index_key, FPGA_num=FPGA_num, bank_num=bank_num)
                 cmd = "./host vadd.xclbin {nlist} {nprobe} {OPQ_enable} {data_dir} /mnt/scratch/wenqi/saved_npy_data/gnd".format(
                     dbname=dbname, nlist=nlist, nprobe=nprobe, OPQ_enable=OPQ_enable, data_dir=data_dir)
